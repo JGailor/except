c081213d4dab73f3daa2d553ba1198882aa2054c
Jeremy Gailor <jeremy@infinitecube.com>
2008-12-28 17:15:01 -0800

    Replaced the find_all with a more performant array subtraction

--- lib/except.rb
+++ lib/except.rb
@@ -7,7 +7,7 @@ module Except
       def find_with_except(*args)
         options = args.last.is_a?(::Hash) ? args.pop : {}
         if exceptions = options.delete(:except)
-          options[:select] = self.column_names.find_all {|name| !exceptions.include?(name)}.join(", ")
+          options[:select] = (self.column_names - exceptions.split(/,\s?/)).join(", ")
         end
         args << options
         find_without_except(*args)
